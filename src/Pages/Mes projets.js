495527c18631caeab1cdb9d27dee1af83802463f
Yoann KAISER <124880029+YoannKAISER@users.noreply.github.com>
2024-02-09 14:04:50 +0100

    Ajout liens vers codes et site sur projets

--- src/Pages/Mes projets.js
+++ src/Pages/Mes projets.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Carrousel from "../Components/Carrousel";
+import { Link } from "react-router-dom";
 
 function MesProjets() {
     return(
         <section>
             <h2>Mes projets réalisés</h2>
             <Carrousel />
+            <Link to="https://github.com/YoannKAISER/Projet12.git" style={{position:"absolute", bottom:"80px", color:"blue", textDecoration:"underline", right:"50px"}}>Codes de mon portfolio</Link>
         </section>
     )
 }
