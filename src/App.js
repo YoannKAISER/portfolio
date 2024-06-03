e511ddee42dbafc049115e0b2f34d5ba2907e662
Yoann KAISER <124880029+YoannKAISER@users.noreply.github.com>
2024-06-03 16:39:56 +0200

    Correction placement speed insight

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import APropos from './Pages/A propos';
 import MesCompétences from './Pages/Mes compétences';
 import MesProjets from './Pages/Mes projets';
 import Contact from './Pages/Contact';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
   return (
     <main className="App">
-    <Routes>
-      <Route path='/' element={<Accueil />} />
-      <Route path='/APropos' element={<APropos />} />
-      <Route path='/Mes compétences' element={<MesCompétences />} />
-      <Route path='/Mes projets' element={<MesProjets />} />
-      <Route path='/Contact' element={<Contact />} />
-    </Routes>
-    
+      <Routes>
+        <Route path='/' element={<Accueil />} />
+        <Route path='/APropos' element={<APropos />} />
+        <Route path='/Mes compétences' element={<MesCompétences />} />
+        <Route path='/Mes projets' element={<MesProjets />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
     </main>
   );
+  <SpeedInsights/>
 };
 
 export default App;
